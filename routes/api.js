016142479060b5b541a51d7eb061b46a6d6de1bf
naraelc <dimentionzero@gmail.com>
2025-01-21 21:18:59 +0900

    apiを追加

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const cors = require('cors');
+router.use(cors());
+
 
 router.get('/hello', (req, res) => {
-  res.json({ message: 'Hello from the standalone backend!!' });
+  res.json({ message: 'Hello from the standalone backend!!!' });
 });
 
+router.get('/goodbye', (req, res) => {
+  res.json({ message: 'Goodbye from the standalone backend!!' });
+});
 module.exports = router;
